3464b95c551d0817c8a0a1eb495e358c0d949747
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-03-23 17:44:27 -0600

    Update Clustering.R

--- Clustering.R
+++ Clustering.R
@@ -9,7 +9,20 @@ library(GGally) #Para hacer el conjunto de graficos
 library(FeatureImpCluster) #Para revisar la importancia de las variables en los grupos.
 library(pheatmap) #Para hacer mapa de calor
 library(tidyverse)
+
+
 db <- readRDS("db10_20.rds")
-View(db)
+#Descripcion de los datos
+set.seed(123)
+db<-data.frame(db)
+db<-db[complete.cases(db),]
 
+EdadHombre<-db[,6]
+EdadMujer<-db[,7]
 
+EdadHombre<-as.numeric(EdadHombre)
+EdadMujer<-as.numeric(EdadMujer)
+
+df<-data.frame(EdadHombre,EdadMujer)
+df[,1:2]<-scale(df[,1:2])
+hopkins(df[,1:2])
