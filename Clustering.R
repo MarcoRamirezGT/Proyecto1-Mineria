241d81986599cf25682920b4dc3fa2326ff82d4a
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-03-23 17:08:18 -0600

    Create Clustering.R

---
+++ Clustering.R
@@ -0,0 +1,15 @@
+library(cluster) #Para calcular la silueta
+library(e1071)#para cmeans
+library(mclust) #mixtures of gaussians
+library(fpc) #para hacer el plotcluster
+library(NbClust) #Para determinar el número de clusters óptimo
+library(factoextra) #Para hacer gráficos bonitos de clustering
+library(hopkins) #Para revisar si vale la pena hacer agrupamiento
+library(GGally) #Para hacer el conjunto de graficos
+library(FeatureImpCluster) #Para revisar la importancia de las variables en los grupos.
+library(pheatmap) #Para hacer mapa de calor
+library(tidyverse)
+db <- readRDS("db10_20.rds")
+View(db)
+
+
