6fb3e4a9e85101c19ef7fdbb6d556d398aca4b7d
Marco Ramirez <46507192+MarcoRamirezGT@users.noreply.github.com>
2022-03-23 19:51:22 -0600

    Cluster

--- Clustering.R
+++ Clustering.R
@@ -8,21 +8,50 @@ library(hopkins) #Para revisar si vale la pena hacer agrupamiento
 library(GGally) #Para hacer el conjunto de graficos
 library(FeatureImpCluster) #Para revisar la importancia de las variables en los grupos.
 library(pheatmap) #Para hacer mapa de calor
-library(tidyverse)
-
 
 db <- readRDS("db10_20.rds")
 #Descripcion de los datos
+#Experimento repetible
 set.seed(123)
-db<-data.frame(db)
-db<-db[complete.cases(db),]
 
-EdadHombre<-db[,6]
-EdadMujer<-db[,7]
+datos<-db[complete.cases(db),]
+summary(datos)
+datos[,6:7]<-scale(datos[,6:7])
+set.seed(123)
+hopkins(datos[,6:7])
+str(datos[,6:7])
 
-EdadHombre<-as.numeric(EdadHombre)
-EdadMujer<-as.numeric(EdadMujer)
 
-df<-data.frame(EdadHombre,EdadMujer)
-df[,1:2]<-scale(df[,1:2])
-hopkins(df[,1:2])
+
+
+
+wss=0
+for (i in 1:10) 
+  wss[i] <- sum(kmeans(db[,6:7], centers=i)$withinss)
+
+plot(1:10, wss, type="b", xlab="Number of Clusters",  ylab="Within groups sum of squares")
+
+fviz_nbclust(datos[,6:7], kmeans, method = "wss") +
+  labs(subtitle = "Elbow method")
+
+fviz_nbclust(datos[,6:7], kmeans, method = "silhouette") +
+  labs(subtitle = "Silhouette method")
+
+#Kmeans
+
+
+
+km<-kmeans(datos[,6:7],3,iter.max =100)
+datos$grupo<-km$cluster
+
+
+plotcluster(datos[,6:7],km$cluster)
+
+
+fviz_cluster(km, data = datos[,6:7],geom = "point", ellipse.type = "norm")
+
+da<-head(datos[6:7],n=(nrow(datos)/10))
+kj<-head(km$cluster,n=(nrow(datos)/10))
+silkm<-silhouette(kj,dist(da))
+
+mean(silkm[,3]) #0.45, no es la mejor particiÃ³n pero no estÃ¡ mal
