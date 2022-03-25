0f036c21458849d6a2e2eada80c9634e7bf46c9f
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-03-24 21:39:33 -0600

    Update

--- ScriptExploratorioER.R
+++ ScriptExploratorioER.R
@@ -1,4 +1,5 @@
 library(tidyverse)
+library(ggplot2)
 
 db <- readRDS("db10_20.rds")
 
@@ -6,17 +7,27 @@ db <- readRDS("db10_20.rds")
 
 
 hombres_ext<-subset(db, select=c('Nacionalidad del hombre'),db$`Nacionalidad del hombre`!=320)
-hombres<-nrow(hombres_ext)
-hombres
+hombres1<-nrow(hombres_ext)
 
 mujeres_ext<-subset(db, select=c('Nacionalidad de la mujer'),db$`Nacionalidad de la mujer`!=320)
-mujeres<-nrow(mujeres_ext)
-mujeres
+mujeres1<-nrow(mujeres_ext)
 
-extranjerosgt<-(mujeres+hombres)
-extranjerosgt
+extran<-data.frame(hombres1, mujeres1)
+extranjerosgtT<-(hombres1 + mujeres1)
 
-df<-data.frame("Hombres Extranjeros"=hombres, "Mujeres extranjeras"=mujeres, "Total"=extranjerosgt)
+
+View(extranjerosgtT)
+
+df<-data.frame("Hombres y Mujeres"=extran, "Total"=extranjerosgtT)
+
+df$Total<-as.factor(df$Total)
+extran$hombres1<-as.factor(df$hombres1)
 
 view(df)
 
+
+ggplot(data=df, aes(x=reorder(extran,extranjerosgtT), y=extranjerosgtT, fill=extran)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  geom_text(aes(label=extran), vjust=1.6, color="black",
+            position = position_dodge(0.9), size=3.5)+
+  labs(title="Cual es del departamento con mas bodas? ", x="Mes de registro", y="Cantidad registrada ")+theme(legend.position="none")
