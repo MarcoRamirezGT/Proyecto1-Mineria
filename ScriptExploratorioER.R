a885297e1b2d6c76f1ed5276cb938674417c49fc
eduardordev <eduardorh1312@gmail.com>
2022-03-24 23:45:08 -0600

    update en rmd

--- ScriptExploratorioER.R
+++ ScriptExploratorioER.R
@@ -1,5 +1,4 @@
 library(tidyverse)
-library(ggplot2)
 
 db <- readRDS("db10_20.rds")
 
@@ -12,22 +11,47 @@ hombres1<-nrow(hombres_ext)
 mujeres_ext<-subset(db, select=c('Nacionalidad de la mujer'),db$`Nacionalidad de la mujer`!=320)
 mujeres1<-nrow(mujeres_ext)
 
-extran<-data.frame(hombres1, mujeres1)
+extran<-c(hombres1, mujeres1)
 extranjerosgtT<-(hombres1 + mujeres1)
 
-
-View(extranjerosgtT)
-
-df<-data.frame("Hombres y Mujeres"=extran, "Total"=extranjerosgtT)
-
-df$Total<-as.factor(df$Total)
-extran$hombres1<-as.factor(df$hombres1)
-
-view(df)
+extraT<-c(extranjerosgtT)
 
 
-ggplot(data=df, aes(x=reorder(extran,extranjerosgtT), y=extranjerosgtT, fill=extran)) +
+df<-data.frame(extran, extraT)
+
+View(df)
+
+df$extran<-as.factor(df$extran)
+
+pregunta1Ed<-ggplot(data=df, aes(x=reorder(extraT,extran), y=extran, fill=extraT)) +
   geom_bar(stat="identity", position=position_dodge())+
   geom_text(aes(label=extran), vjust=1.6, color="black",
             position = position_dodge(0.9), size=3.5)+
-  labs(title="Cual es del departamento con mas bodas? ", x="Mes de registro", y="Cantidad registrada ")+theme(legend.position="none")
+  labs(title="Cantidad de extranjeros registrados en Guatemala ", x="Total de Extranjeros", y="Datos Hombres y Mujeres")+theme(legend.position="none")
+
+
+# Maximo de bodas registradas en un dia
+dep<-db[,'Dia de ocurrencia']
+depocu<-subset(dep, select=c('Dia de ocurrencia'),db$`Dia de ocurrencia` != 0)
+dep1<-data.frame(depocu)
+nde<-aggregate(dep1$dep, dep1, length)
+
+View(nde)
+
+
+D<-c(nde$depocu)
+C<-c(nde$x)
+df<-data.frame(D,C)
+df$D<-as.factor(df$D)
+ask1<-df[order(-df$C),]
+ask1f<-head(ask1,n=5)
+
+
+pregunta2ED<-ggplot(data=ask1f, aes(x=reorder(D,-C), y=C, fill=D)) +
+  geom_bar(stat="identity", position=position_dodge())+
+  geom_text(aes(label=C), vjust=1.6, color="black",
+            position = position_dodge(0.9), size=3.5)+
+  labs(title="Maximo de bodas registradas en un dia ", x="Dia", y="Cantidad de bodas")+theme(legend.position="none")
+
+
+
