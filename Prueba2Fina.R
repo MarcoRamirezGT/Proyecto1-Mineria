4eb57c2fe7e04e73d468e0c0a3f813138bf1c7a2
AQ-ja <53914116+AQ-ja@users.noreply.github.com>
2022-05-20 22:14:42 -0600

    Update

--- Prueba2Fina.R
+++ Prueba2Fina.R
@@ -10,7 +10,7 @@ data <- readRDS("db10_20.rds")
 str(data)
 
 
-df<-data[,c("Departamento de registro","Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
+df<-data[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
 
 ws<-data[,c("Clase de union")]
 
@@ -20,5 +20,31 @@ m1<-rpart(ws~ ., data = df, method = "anova")
 m1
 rpart.plot(m1, type = 3, digits = 3, fallen.leaves = TRUE )
 
+
+#Precision o matriz
+
+train<-data
+porciento<-0.7
+
+datos <- train[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
+datosFiltertree <- data[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
+datosFiltertree
+
+
+set.seed(123)
+trainRowsNumber <- sample(1:nrow(datosFiltertree),porciento*nrow(datosFiltertree))
+train<-datosFiltertree[trainRowsNumber,]
+test<-datosFiltertree[-trainRowsNumber,]
+
+modeloRF1<-randomForest(train$`Clase de union`~.,train)
+prediccionRF1<-predict(modeloRF1,newdata = test)
+testCompleto<-test
+testCompleto$predRF<-prediccionRF1
+testCompleto$predRF<-round(testCompleto$predRF)
+cfmRandomForest <- table(testCompleto$predRF, testCompleto$`Clase de union`)
+plot(cfmRandomForest);text(cfmRandomForest)
+
+cfmRandomForest <- confusionMatrix(table(testCompleto$predRF, testCompleto$`Clase de union`))
+cfmRandomForest
 
 
