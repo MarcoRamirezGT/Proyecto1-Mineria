5433226fd9b50e2985c9dff7da6842988c60b6eb
Estuardo07 <xavihernand7@hotmail.com>
2022-05-21 00:22:31 -0600

    Update

--- Prueba2Fina.R
+++ Prueba2Fina.R
@@ -5,30 +5,25 @@ library(tree)
 library(rpart.plot)
 library(randomForest)
 
-data <- readRDS("db10_20.rds")
 
-str(data)
+data <- readRDS("db10_20.rds")
 
 
 df<-data[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
 
 ws<-data[,c("Clase de union")]
-
 ws<- haven::as_factor(ws)
 
 m1<-rpart(ws~ ., data = df, method = "anova")
-m1
 rpart.plot(m1, type = 3, digits = 3, fallen.leaves = TRUE )
 
 
 #Precision o matriz
-
 train<-data
 porciento<-0.7
 
 datos <- train[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
 datosFiltertree <- data[,c("Municipio de registro","Escolaridad del hombre","Escolaridad de la mujer","Grupo etnico del hombre","Grupo etnico de la mujer", "Clase de union")]
-datosFiltertree
 
 
 set.seed(123)
@@ -46,5 +41,3 @@ plot(cfmRandomForest);text(cfmRandomForest)
 
 cfmRandomForest <- confusionMatrix(table(testCompleto$predRF, testCompleto$`Clase de union`))
 cfmRandomForest
-
-
