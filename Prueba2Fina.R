8e122777aa26ec861f3fe9d3dc4d5ad56012e17d
MarcoRamirezGT <maquito156@gmail.com>
2022-05-23 00:25:56 -0600

    table

--- Prueba2Fina.R
+++ Prueba2Fina.R
@@ -40,7 +40,7 @@ expected_value = factor(test$`Clase de union`)
 #Igualar los niveles
 #levels(expected_value) <- levels(predicted_value)
 
-
+table(expected_value,predicted_value)
 confusionMatrix(expected_value,predicted_value)
 Accuracy(predicted_value,expected_value)
 #confusionMatrix <- confusionMatrix(data=predicted_value, reference = expected_value)
